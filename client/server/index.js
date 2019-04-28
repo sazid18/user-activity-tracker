53f491cc2d4971ee095566d454df1735bbc69534
Md Sazid Khan <mdsazidkhan18@gmail.com>
2019-04-28 13:58:16 +0530

    Resolving Cors error
    required morgan and body parser.

--- client/server/index.js
+++ client/server/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const morgan = require("morgan");
 
 app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended : true }));
 
 // Connect to DB
-mongoose.connect('mongodb+srv://sazid:hzNsbtLhaYmSXBAj@cluster0-zaynt.mongodb.net/User_Activity?retryWrites=true');
+mongoose.connect('mongodb+srv://sazid:cMTQU1ZkB465usLD@cluster0-ww20z.mongodb.net/User_Activity?retryWrites=true');
 let db=mongoose.connection;
 
 db.once('open',()=>{
@@ -21,6 +26,7 @@ const myModel = require('./models/data.js');
 
 // Save to DB
 app.post('/save', (req, res) => {
+	  console.log(req.body);
     const obj = new myModel({
         userEvent : req.body.userEvent,
         image : req.body.image
